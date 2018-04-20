3e1bd28e4a4faa678ae13713161ca3019d9ebfe7
Brett Kuskie <fullaxx@gmail.com>
2018-04-20 14:46:43 +0000

    updated 009-xmodlibs/03c-clean_pinentry.sh to fix missing symlink /usr/bin/pinentry -> pinentry-curses when GTK+2 is not available

--- RapidBuild64/009-xmodlibs/03c-clean_pinentry.sh
+++ RapidBuild64/009-xmodlibs/03c-clean_pinentry.sh
@@ -8,6 +8,8 @@ if [ "${INCPINENTRY}" == "Y" ]; then
   rm ${DIR}/usr/bin/pinentry-gnome3
   if [ "${INCGTKTWO}" != "Y" ]; then
     rm ${DIR}/usr/bin/pinentry-gtk-2
+    rm ${DIR}/usr/bin/pinentry
+    ln -s pinentry-curses ${DIR}/usr/bin/pinentry
   fi
   if [ "${INCQT}" != "Y" ]; then
     rm ${DIR}/usr/bin/pinentry-qt
