abde19a436638752e8600e85d861df93a04ff279
Brett Kuskie <fullaxx@gmail.com>
2018-05-01 12:59:38 +0000

    removed clean script and updated RapidFileserver64/003-interp with new packages.cfg.sh

--- RapidFileserver64/003-interp/packages.cfg.sh
+++ RapidFileserver64/003-interp/packages.cfg.sh
@@ -1,1 +1,11 @@
-../basis/003-interp/packages.cfg.sh
+# 003-interp options
+if [ "${INCMOD003INTERP}" == "Y" ]; then
+  INCPERL="N"
+  INCPYTHON="Y"
+  INCRUBY="N"
+  INCLUA="N"
+  INCTCL="N"
+
+# just about everything needs db48
+  INCDB48="Y"
+fi
