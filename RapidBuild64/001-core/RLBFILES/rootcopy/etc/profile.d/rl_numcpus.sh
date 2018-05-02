ca569dc9f81f0f0669799ce5f21eb9345e750783
Brett Kuskie <fullaxx@gmail.com>
2018-05-02 16:28:29 +0000

    updated rl_numcpus.sh with nproc

--- RapidBuild64/001-core/RLBFILES/rootcopy/etc/profile.d/rl_numcpus.sh
+++ RapidBuild64/001-core/RLBFILES/rootcopy/etc/profile.d/rl_numcpus.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-NUMCPUS=`lscpu | grep 'CPU(s):' | grep -v NUMA | awk '{print $2}'`
-export MAKEALLCPUS="-j$NUMCPUS"
+# NUMCPUS=`lscpu | grep 'CPU(s):' | grep -v NUMA | awk '{print $2}'`
+# export MAKEALLCPUS="-j$NUMCPUS"
+
+NUMCPUS=`nproc`
+export MAKEALLCPUS="-j${NUMCPUS}"
