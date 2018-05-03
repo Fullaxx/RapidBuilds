69b3162dfbfe97af21ec30e904114cd728bc680a
Brett Kuskie <fullaxx@gmail.com>
2018-05-03 14:41:15 +0000

    added build_all.sh

---
+++ build_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ `id -u` != "0" ]; then
+  echo "Got Root?"
+  exit 1
+fi
+
+RAPIDBUILDSDIR=`dirname $0`
+
+${RAPIDBUILDSDIR}/RapidBuild64/rebuild.sh all
+${RAPIDBUILDSDIR}/RapidInstall64/rebuild.sh all
+${RAPIDBUILDSDIR}/RapidFileserver64/rebuild.sh all
+${RAPIDBUILDSDIR}/RapidWorkstation64/rebuild.sh all
