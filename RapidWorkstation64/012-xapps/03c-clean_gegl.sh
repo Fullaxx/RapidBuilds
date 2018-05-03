7c7c3b3cc186ad3781424c841f04c5cef1442be5
Brett Kuskie <fullaxx@gmail.com>
2018-05-03 13:26:56 +0000

    updated 03c-clean_gegl.sh

--- RapidWorkstation64/012-xapps/03c-clean_gegl.sh
+++ RapidWorkstation64/012-xapps/03c-clean_gegl.sh
@@ -6,4 +6,5 @@ source ./module_spec.sh
 # Clean up gegl if we dont have ffmpeg
 if [ "${INCGEGL}" == "Y" -a "${INCFFMPEG}" != "Y" ]; then
   rm ${DIR}/usr/${LIBDIR}/gegl-?.?/ff-load.{so,la}
+  rm ${DIR}/usr/${LIBDIR}/gegl-?.?/ff-save.{so,la}
 fi
