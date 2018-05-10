ed101f532d6ca6dddcc6d84ca92251831f25a1e3
Brett Kuskie <fullaxx@gmail.com>
2018-05-10 13:23:30 +0000

    put back nano,parted,tree in RapidInstall64/001-core/01b-trim_pkgs.sh

--- RapidInstall64/001-core/01b-trim_pkgs.sh
+++ RapidInstall64/001-core/01b-trim_pkgs.sh
@@ -16,17 +16,14 @@ rm kbd-*.txz
 rm libssh2-*.txz
 rm lzo-*.txz
 rm mpfr-*.txz
-rm nano-*.txz
 rm ncurses-*.txz
 rm openssh-*.txz
-rm parted-*.txz
 rm popt-*.txz
 rm reiserfsprogs-*.txz
 rm screen-*.txz
 rm squashfs-tools-*.txz
 rm sysfsutils-*.txz
 rm tar-*.txz
-rm tree-*.txz
 rm utempter-*.txz
 rm xfsdump-*.txz
 rm xfsprogs-*.txz
