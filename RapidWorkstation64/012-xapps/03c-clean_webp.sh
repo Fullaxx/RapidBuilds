3ea1528e2f0a8fefcb0ed709bdbc16e8817c5b67
Brett Kuskie <fullaxx@gmail.com>
2018-05-11 10:44:53 +0000

    fixed INCLIBGIF -> INCGIFLIB in RapidWorkstation64/012-xapps/03c-clean_webp.sh

--- RapidWorkstation64/012-xapps/03c-clean_webp.sh
+++ RapidWorkstation64/012-xapps/03c-clean_webp.sh
@@ -4,6 +4,6 @@ source ../spec.sh
 source ./module_spec.sh
 
 # Clean up webp
-if [ "${INCLIBWEBP}" == "Y" -a "${INCLIBGIF}" != "Y" ]; then
+if [ "${INCLIBWEBP}" == "Y" -a "${INCGIFLIB}" != "Y" ]; then
   rm ${DIR}/usr/bin/gif2webp
 fi
