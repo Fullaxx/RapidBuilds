e60297d20ff385241c8d5d72e14bf73c4ffc7c6a
Brett Kuskie <fullaxx@gmail.com>
2018-05-13 13:16:36 +0000

    updated IntelLaptop64 with new baseoutput

---
+++ IntelLaptop64/baseoutput/04m-install_modules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ../spec.sh
+source ./module_spec.sh
+set -e
+
+if [ ! -d ${MODSDIR} ]; then
+  echo "${MODSDIR} deos not exist!"
+  exit 1
+fi
+
+# Copy any extra modules
+cp -Lpv ${PACKAGESDIR}/java8/jre-8u172-x86_64-1.xzm ${MODSDIR}/
+cp -Lpv ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.10-x86_64-1.xzm ${MODSDIR}/
+cp -Lpv ${PACKAGESDIR}/LibreOffice/LibreOffice-5.3.7-x86_64-2.xzm ${MODSDIR}/
