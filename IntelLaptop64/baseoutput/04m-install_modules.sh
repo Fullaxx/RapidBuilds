5a128a9088a797e0988305274c4fcf8ec8f93036
Brett Kuskie <fullaxx@gmail.com>
2018-05-15 14:39:49 +0000

    updated VirtualBox to 5.2.12 in IntelLaptop64/baseoutput/04m-install_modules.sh

--- IntelLaptop64/baseoutput/04m-install_modules.sh
+++ IntelLaptop64/baseoutput/04m-install_modules.sh
@@ -11,5 +11,5 @@ fi
 
 # Copy any extra modules
 cp -Lpv ${PACKAGESDIR}/java8/jre-8u172-x86_64-1.xzm ${MODSDIR}/
-cp -Lpv ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.10-x86_64-1.xzm ${MODSDIR}/
+cp -Lpv ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.12${ARCH}-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/LibreOffice/LibreOffice-5.3.7-x86_64-2.xzm ${MODSDIR}/
