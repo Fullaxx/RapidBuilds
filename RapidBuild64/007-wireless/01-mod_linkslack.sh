876a1536879890979fc6d945c89c7a74716377e7
Brett Kuskie <fullaxx@gmail.com>
2018-05-24 20:23:26 +0000

    updated RapidBuild64/007-wireless

--- RapidBuild64/007-wireless/01-mod_linkslack.sh
+++ RapidBuild64/007-wireless/01-mod_linkslack.sh
@@ -8,27 +8,33 @@ rm -f *.txz *.xzm
 ln -s ${SLACKPKGDIR}/n/iw-*.txz
 ln -s ${SLACKPKGDIR}/n/wireless_tools-*.txz
 
-if [ "$INCIPWFIRMWARE" == "Y" ]; then
-  for PKG in ${SLACKPKGDIR}/n/ipw*.txz; do ln -s $PKG; done
-fi
+if [ "${INCBLUETOOTH}" == "Y" ]; then
 
-if [ "$INCZDFIRMWARE" == "Y" ]; then
-  for PKG in ${SLACKPKGDIR}/n/zd*.txz; do ln -s $PKG; done
-fi
-
-if [ "$INCWPASUPPLICANT" == "Y" ]; then
-  ln -s ${SLACKPKGDIR}/n/wpa_supplicant-*.txz
-fi
-
-if [ "$INCBLUETOOTH" == "Y" ]; then
-
-  for PKG in ${SLACKPKGDIR}/n/bluez-*.txz; do ln -s $PKG; done
+  for PKG in ${SLACKPKGDIR}/n/bluez-*.txz; do ln -s ${PKG}; done
 
 # BlueDevil is a set of components which integrate Bluetooth in KDE
-  if [ "$INCQT" == "Y" ]; then
+  if [ "${INCQT}" == "Y" ]; then
     ln -s ${SLACKPKGDIR}/l/libbluedevil-*.txz
   fi
 
 fi
 
-# linkmodule ${SLACKEXTRA}/wicd/wicd-*.txz
+if [ "${INCCRDA}" == "Y" ]; then
+  ln -s ${SLACKPKGDIR}/n/crda-*.txz
+fi
+
+if [ "${INCIPWFIRMWARE}" == "Y" ]; then
+  for PKG in ${SLACKPKGDIR}/n/ipw*.txz; do ln -s ${PKG}; done
+fi
+
+if [ "${INCWICD}" == "Y" ]; then
+  linkmodule ${SLACKEXTRA}/wicd/wicd-*.txz
+fi
+
+if [ "${INCWPASUPPLICANT}" == "Y" ]; then
+  ln -s ${SLACKPKGDIR}/n/wpa_supplicant-*.txz
+fi
+
+if [ "${INCZDFIRMWARE}" == "Y" ]; then
+  for PKG in ${SLACKPKGDIR}/n/zd*.txz; do ln -s ${PKG}; done
+fi
