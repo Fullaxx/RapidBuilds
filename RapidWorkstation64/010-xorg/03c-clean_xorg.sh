f8599906d1d6169715871cd104b66b70911eb192
Brett Kuskie <fullaxx@gmail.com>
2018-05-24 20:24:01 +0000

    updated RapidWorkstation64/010-xorg/03c-clean_xorg.sh to reflect new xorg-server-1.20.0 package

--- RapidWorkstation64/010-xorg/03c-clean_xorg.sh
+++ RapidWorkstation64/010-xorg/03c-clean_xorg.sh
@@ -7,5 +7,6 @@ source ./module_spec.sh
 if [ "${INCMESA}" != "Y" ]; then
   rm ${DIR}/usr/${LIBDIR}/xorg/modules/extensions/libglx.so
   rm ${DIR}/usr/${LIBDIR}/xorg/modules/libglamoregl.so
-  rm ${DIR}/usr/bin/Xfbdev
+#  /usr/bin/Xfbdev is gone as of xorg-server-1.20.0 ?
+#  rm ${DIR}/usr/bin/Xfbdev
 fi
