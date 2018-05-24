d9ddcf29969aaf79aaf49eb0826acc6f953cbc2b
Brett Kuskie <fullaxx@gmail.com>
2018-05-24 14:27:24 +0000

    updated RapidWorkstation64/012-xapps/03d-default_de.sh with openbox and enlightenment values

--- RapidWorkstation64/012-xapps/03d-default_de.sh
+++ RapidWorkstation64/012-xapps/03d-default_de.sh
@@ -5,8 +5,10 @@ source ./module_spec.sh
 
 # Set a defaukt DE based on the value in ${DEFAULTDE}
 case "${DEFAULTDE}" in
-   "fluxbox") ln -s xinitrc.fluxbox  ${DIR}/etc/X11/xinit/xinitrc ;;
-  "blackbox") ln -s xinitrc.blackbox ${DIR}/etc/X11/xinit/xinitrc ;;
-      "xfce") ln -s xinitrc.xfce     ${DIR}/etc/X11/xinit/xinitrc ;;
-       "kde") ln -s xinitrc.kde      ${DIR}/etc/X11/xinit/xinitrc ;;
+     "blackbox") ln -s xinitrc.blackbox      ${DIR}/etc/X11/xinit/xinitrc ;;
+"enlightenment") ln -s xinitrc.enlightenment ${DIR}/etc/X11/xinit/xinitrc ;;
+      "fluxbox") ln -s xinitrc.fluxbox       ${DIR}/etc/X11/xinit/xinitrc ;;
+      "openbox") ln -s xinitrc.openbox       ${DIR}/etc/X11/xinit/xinitrc ;;
+         "xfce") ln -s xinitrc.xfce          ${DIR}/etc/X11/xinit/xinitrc ;;
+          "kde") ln -s xinitrc.kde           ${DIR}/etc/X11/xinit/xinitrc ;;
 esac
