5203fc8b0cc8fd20723ea0de28ac675f9b68d6ff
Brett Kuskie <fullaxx@gmail.com>
2018-06-12 11:29:33 +0000

    before ISO creation, set chown root.root

--- RapidBuild64/baseoutput/05-stamp_iso.sh
+++ RapidBuild64/baseoutput/05-stamp_iso.sh
@@ -11,6 +11,9 @@ else
   ISOFILENAME="${PROJNAME}.iso"
 fi
 
+# Set Permissions
+chown -R root.root ${SLAXDIR}
+
 # Build the ISO
 ( cd ${SLAXDIR}; ./make_iso.sh ${ISOOUTPUTDIR}/${ISOFILENAME} && echo "${ISOOUTPUTDIR}/${ISOFILENAME} created" || echo "ISO CREATION FAILED!" )
 
