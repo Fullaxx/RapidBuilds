79dddeb72e78bfecdd585c343d4c13aa3501bfee
Brett Kuskie <fullaxx@gmail.com>
2018-06-13 09:36:25 +0000

    updated packages.?.cfg.sh in RapidFileserver64

--- RapidFileserver64/003-interp/packages.2.cfg.sh
+++ RapidFileserver64/003-interp/packages.2.cfg.sh
@@ -1,11 +1,6 @@
 # 003-interp options
 if [ "${INCMOD003INTERP}" == "Y" ]; then
   INCPERL="N"
-  INCPYTHON="Y"
-  INCRUBY="N"
-  INCLUA="N"
-  INCTCL="N"
 
-# just about everything needs db48
-  INCDB48="Y"
+  INCPYTHONSETUPTOOLS="N"
 fi
