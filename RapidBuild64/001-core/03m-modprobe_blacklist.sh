f00cfc9f84daf89eb316b16a0945259a997185f8
Brett Kuskie <fullaxx@gmail.com>
2018-06-19 13:32:50 -0600

    updated modprobe blacklist with comments

--- RapidBuild64/001-core/03m-modprobe_blacklist.sh
+++ RapidBuild64/001-core/03m-modprobe_blacklist.sh
@@ -3,16 +3,20 @@
 source ../spec.sh
 source ./module_spec.sh
 
+echo "# Graphics" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist ast" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist nouveau" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist mgag200" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist mga" >> ${DIR}/etc/modprobe.d/blacklist.conf
 
-# echo "blacklist ipv6" >> ${DIR}/etc/modprobe.d/blacklist.conf
-
+echo >> ${DIR}/etc/modprobe.d/blacklist.conf
+echo "# Networking" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist i2400m" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist i2400m_usb" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist wimax" >> ${DIR}/etc/modprobe.d/blacklist.conf
+# echo "blacklist ipv6" >> ${DIR}/etc/modprobe.d/blacklist.conf
 
+echo >> ${DIR}/etc/modprobe.d/blacklist.conf
+echo "# Old Devices" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist parport_pc" >> ${DIR}/etc/modprobe.d/blacklist.conf
 echo "blacklist parport" >> ${DIR}/etc/modprobe.d/blacklist.conf
