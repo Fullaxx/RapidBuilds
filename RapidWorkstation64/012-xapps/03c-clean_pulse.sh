a48643d8f5f4cd8a252c3a7c21638eed20d94af7
Brett Kuskie <fullaxx@gmail.com>
2018-06-19 23:02:47 +0000

    enable pusleaudio daemon on startup

--- RapidWorkstation64/012-xapps/03c-clean_pulse.sh
+++ RapidWorkstation64/012-xapps/03c-clean_pulse.sh
@@ -3,7 +3,15 @@
 source ../spec.sh
 source ./module_spec.sh
 
+# If we have pulse, lets use it
+# Chrome and Vivaldi wont have sound w/o it - FF will??
+if [ "${INCPULSE}" == "Y" ]; then
+  ls -l ${DIR}/etc/rc.d/rc.pulseaudio
+  chmod 0755 ${DIR}/etc/rc.d/rc.pulseaudio
+  ls -l ${DIR}/etc/rc.d/rc.pulseaudio
+
 # Clean up pulse, if we dont have GCONF
-if [ "${INCPULSE}" == "Y" -a "${INCGCONF}" != "Y" ]; then
-  rm ${DIR}/usr/libexec/pulse/gconf-helper
+  if [ "${INCGCONF}" != "Y" ]; then
+    rm ${DIR}/usr/libexec/pulse/gconf-helper
+  fi
 fi
