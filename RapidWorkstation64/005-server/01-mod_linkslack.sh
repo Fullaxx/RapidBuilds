a483bd6e04c8af313305c2ed74e28297486af8f2
Brett Kuskie <fullaxx@gmail.com>
2018-06-23 13:12:51 +0000

    added bind,dnsmasq to RapidWorkstation64/005-server

--- RapidWorkstation64/005-server/01-mod_linkslack.sh
+++ RapidWorkstation64/005-server/01-mod_linkslack.sh
@@ -5,6 +5,18 @@ source ./module_spec.sh
 
 rm -f *.txz *.xzm
 
+if [ "${INCAPACHEHTTPD}" == "Y" ]; then
+  linkpackage ${SLACKPKGDIR}/n/httpd-*.txz
+fi
+
+if [ "${INCBIND}" == "Y" ]; then
+  linkpackage ${SLACKPKGDIR}/n/bind-*.txz
+fi
+
+if [ "${INCDNSMASQ}" == "Y" ]; then
+  linkpackage ${SLACKPKGDIR}/n/dnsmasq-*.txz
+fi
+
 if [ "${INCLXC}" == "Y" ]; then
   linkpackage ${SLACKPKGDIR}/ap/lxc-*.txz
   linkpackage ${SLACKPKGDIR}/a/libcgroup-*.txz
@@ -14,24 +26,20 @@ if [ "${INCMARIADB}" == "Y" ]; then
   linkpackage ${SLACKPKGDIR}/ap/mariadb-*.txz
 fi
 
-if [ "${INCPHP}" == "Y" ]; then
-  linkpackage ${SLACKPKGDIR}/n/php-*.txz
-fi
-
-if [ "${INCSAMBA}" == "Y" ]; then
-  linkpackage ${SLACKPKGDIR}/n/samba-*.txz
-fi
-
 if [ "${INCNETSNMP}" == "Y" ]; then
   linkpackage ${SLACKPKGDIR}/n/net-snmp-*.txz
 fi
 
-if [ "${INCAPACHEHTTPD}" == "Y" ]; then
-  linkpackage ${SLACKPKGDIR}/n/httpd-*.txz
+if [ "${INCPHP}" == "Y" ]; then
+  linkpackage ${SLACKPKGDIR}/n/php-*.txz
 fi
 
 if [ "${INCPROFTPD}" == "Y" ]; then
   linkpackage ${SLACKPKGDIR}/n/proftpd-*.txz
+fi
+
+if [ "${INCSAMBA}" == "Y" ]; then
+  linkpackage ${SLACKPKGDIR}/n/samba-*.txz
 fi
 
 if [ "${INCVSFTPD}" == "Y" ]; then
