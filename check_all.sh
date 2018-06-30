fe77347b08203d5c2348ce9cd2d7ccf44972f54e
Brett Kuskie <fullaxx@gmail.com>
2018-06-30 14:48:01 -0600

    added check_all.sh: script to check for any broken links in your projects

---
+++ check_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+#if [ `id -u` != "0" ]; then
+#  echo "Got Root?"
+#  exit 1
+#fi
+
+RETVAL=0
+RAPIDBUILDSDIR=`dirname $0`
+PROJECTS="RapidBuild64 RapidInstall64 RapidFileserver64 RapidWorkstation64 IntelLaptop64"
+
+for PROJDIR in ${PROJECTS}; do
+  for SCRLINK in `find ${RAPIDBUILDSDIR}/${PROJDIR} -type l -name "*.sh"`; do
+    if [ ! -e ${SCRLINK} ]; then
+      RETVAL=1
+      ls -l ${SCRLINK}
+    fi
+  done
+done
+
+exit ${RETVAL}
