dfa9b05a145d062ab474af6138a91b1a5edead30
Brett Kuskie <fullaxx@gmail.com>
2018-08-09 04:21:28 -0600

    added 009-xmodlibs/03c-clean_gnutls.sh to clean guile from gnutls if not installed

---
+++ RapidBuild64/009-xmodlibs/03c-clean_gnutls.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ../spec.sh
+source ./module_spec.sh
+
+# Clean up gnutls
+# if [ "${INCGNUTLS}" == "Y" ]; then - doesnt exist yet
+  if [ "${INCGUILE}" != "Y" ]; then
+    rm -r ${DIR}/usr/lib64/guile ${DIR}/usr/share/guile
+  fi
+# fi
