941d667f112d9a4efc50af22f46fb7ff96d8e2a6
Brett Kuskie <fullaxx@gmail.com>
2018-08-15 12:02:30 -0600

    updated CudaWorkstation64/baseoutput/04m-install_modules.sh

--- CudaWorkstation64/baseoutput/04m-install_modules.sh
+++ CudaWorkstation64/baseoutput/04m-install_modules.sh
@@ -11,11 +11,14 @@ fi
 # Copy any extra modules that don't need RDL
 cp -Lpv ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.16-${ARCH}-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/LibreOffice/LibreOffice-5.4.7-${ARCH}-1.xzm ${MODSDIR}/
-cp -Lpv ${PACKAGESDIR}/chrome/chrome-latest-${ARCH}.xzm ${MODSDIR}/
-# cp -Lpv ${PACKAGESDIR}/chromium/chromium-64.0.3282.119-${ARCH}-1alien.xzm ${MODSDIR}/
+cp -Lpv ${PACKAGESDIR}/chrome/chrome-67.0.3396.99-${ARCH}-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/dbwalls/dbwalls-1920-130207.xzm ${MODSDIR}/
-cp -Lpv ${PACKAGESDIR}/java8/jre-8u172-${ARCH}-1.xzm ${MODSDIR}/
+cp -Lpv ${PACKAGESDIR}/java8/jdk-8u181-${ARCH}-1.xzm ${MODSDIR}/
+cp -Lpv ${PACKAGESDIR}/java8/jdkdocs-8u181-noarch-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/nvidia_driver/011-nvidia-390.77-4.9.112.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda-9.2.88.1-${ARCH}-1.xzm ${MODSDIR}/
+
+# cp -Lpv ${PACKAGESDIR}/chrome/chrome-latest-${ARCH}.xzm ${MODSDIR}/
+# cp -Lpv ${PACKAGESDIR}/chromium/chromium-64.0.3282.119-${ARCH}-1alien.xzm ${MODSDIR}/
 # cp -Lpv ${PACKAGESDIR}/veracrypt/veracrypt-gui-latest-${ARCH}.xzm ${MODSDIR}/
 # cp -Lpv ${PACKAGESDIR}/vivaldi/vivaldi-latest-${ARCH}.xzm ${MODSDIR}/
