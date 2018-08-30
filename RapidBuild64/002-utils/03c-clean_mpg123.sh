82772c260771b53e60cf25f8c47fb32445826663
Brett Kuskie <fullaxx@gmail.com>
2018-08-30 08:17:15 -0600

    updated RapidBuild64/002-utils/03c-clean_mpg123.sh to not clean .la files since they are no longer included

--- RapidBuild64/002-utils/03c-clean_mpg123.sh
+++ RapidBuild64/002-utils/03c-clean_mpg123.sh
@@ -5,10 +5,10 @@ source ./module_spec.sh
 
 # clean up mpg123 if we don't have pulse
 if [ "${INCMPGONETWOTHREE}" == "Y" -a "${INCPULSE}" != "Y" ]; then
-  rm ${DIR}/usr/${LIBDIR}/mpg123/output_pulse.{la,so}
+  rm ${DIR}/usr/${LIBDIR}/mpg123/output_pulse.so
 fi
 
 # clean up mpg123 if we don't have sdl
 if [ "${INCMPGONETWOTHREE}" == "Y" -a "${INCSDL}" != "Y" ]; then
-  rm ${DIR}/usr/${LIBDIR}/mpg123/output_sdl.{la,so}
+  rm ${DIR}/usr/${LIBDIR}/mpg123/output_sdl.so
 fi
