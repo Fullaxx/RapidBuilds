782eb0b23c09b61ee9d9a07e39db546cc7983819
Brett Kuskie <fullaxx@gmail.com>
2018-08-30 09:44:47 -0600

    added custom packages to CudaWorkstation64/012-xapps

---
+++ CudaWorkstation64/012-xapps/packages.2.cfg.sh
@@ -0,0 +1,40 @@
+# 012-xapps options
+if [ "${INCMOD012XAPPS}" == "Y" ]; then
+# Desktop ENV
+  INCFLUXBOX="Y"
+  INCOPENBOX="Y"
+
+# Set a default DE, if you desire
+  DEFAULTDE="openbox"
+
+# Apps
+  INCAUDACIOUS="Y"
+  INCGIMP="Y"
+  INCGKRELLM="Y"
+  INCGPARTED="Y"
+  INCMOZILLAFIREFOX="Y"
+  INCMOZILLATHUNDERBIRD="N"
+  INCMPLAYER="N"
+  INCRDESKTOP="Y"
+  INCRXVTUNICODE="Y"
+  INCSOMA="N"
+  INCXINE="N"
+  INCXLOCK="Y"
+  INCXPAINT="Y"
+
+# extra family of packages
+  INCTIGERVNC="Y"
+
+# Custom Additions
+  INCBLUEFISH="Y"
+  INCBLUEFISHTWO="N"
+  INCETERM="Y"
+  INCEVINCE="Y"
+  INCFBPANEL="Y"
+  INCGEANY="Y"
+  INCHSETROOT="Y"
+  INCOBCONF="Y"
+  INCXFE="Y"
+
+  INCCHROME="Y"
+fi
