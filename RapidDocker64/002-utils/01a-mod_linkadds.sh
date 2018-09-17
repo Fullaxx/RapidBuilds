9e90c945537b29a351e81dc269681018e71bceed
Brett Kuskie <fullaxx@gmail.com>
2018-09-17 10:28:10 -0600

    added dnsmasq to RapidDocker64/002-utils

--- RapidDocker64/002-utils/01a-mod_linkadds.sh
+++ RapidDocker64/002-utils/01a-mod_linkadds.sh
@@ -4,6 +4,7 @@ source ../spec.sh
 source ./module_spec.sh
 
 linkpackage ${SLACKPKGDIR}/d/git-*.txz
+linkpackage ${SLACKPKGDIR}/n/dnsmasq-*.txz
 
 # Docker is currently provided in the shell
 # linkpackage "${PACKAGESDIR}/docker/docker-18.06.0-ce-x86_64-1.xzm"
