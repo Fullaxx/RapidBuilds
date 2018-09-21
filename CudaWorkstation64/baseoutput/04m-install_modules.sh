fe1ffb5423fc0f8f4919cdac30f495764eb51674
Brett Kuskie <fullaxx@gmail.com>
2018-09-21 10:02:34 -0600

    updated Cuda for CudaWorkstation64

--- CudaWorkstation64/baseoutput/04m-install_modules.sh
+++ CudaWorkstation64/baseoutput/04m-install_modules.sh
@@ -39,7 +39,7 @@ cp -Lpv ${PACKAGESDIR}/dbwalls/dbwalls-1920-130207.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/java8/jdkdocs-8u181-noarch-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/java8/jdk-8u181-${ARCH}-3.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/netbeans/netbeans-8.1.0.xzm ${MODSDIR}/
-cp -Lpv ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda-9.2.148.1-${ARCH}-1.xzm ${MODSDIR}/
+cp -Lpv ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda-9.2.148.1-${ARCH}-2.xzm ${MODSDIR}/
 
 # cp -Lpv ${PACKAGESDIR}/chromium/chromium-64.0.3282.119-${ARCH}-1alien.xzm ${MODSDIR}/
 # cp -Lpv ${PACKAGESDIR}/veracrypt/veracrypt-gui-latest-${ARCH}.xzm ${MODSDIR}/
