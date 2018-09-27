130a3762822825072ab6a2d5ca8209268b02afdc
Brett Kuskie <fullaxx@gmail.com>
2018-09-27 14:03:23 -0600

    Removed unnecessary checking in CudaWorkstation64/baseoutput/04m-install_modules.sh

--- CudaWorkstation64/baseoutput/04m-install_modules.sh
+++ CudaWorkstation64/baseoutput/04m-install_modules.sh
@@ -2,8 +2,6 @@
 
 source ../spec.sh
 source ./module_spec.sh
-NVDDVERS="390.87"
-#CHROMEVERS="69.0.3497.100"
 
 if [ ! -d ${BASEDIR} ]; then
   echo "${BASEDIR} does not exist!"
@@ -15,12 +13,7 @@ if [ ! -d ${MODSDIR} ]; then
   exit 1
 fi
 
-if [ ! -e ${PACKAGESDIR}/nvidia_driver/011-nvidia-${NVDDVERS}-${KERNVERS}.xzm ]; then
-  echo "${PACKAGESDIR}/nvidia_driver/011-nvidia-${NVDDVERS}-${KERNVERS}.xzm does not exist!"
-  exit 1
-fi
-
-cp -Lpv ${PACKAGESDIR}/nvidia_driver/011-nvidia-${NVDDVERS}-${KERNVERS}.xzm ${BASEDIR}/
+cp -Lpv ${PACKAGESDIR}/nvidia_driver/011-nvidia-390.87-${KERNVERS}.xzm ${BASEDIR}/
 
 # Chrome needs RDL
 if [ "${INCCHROME}" == "Y" ]; then
@@ -42,7 +35,3 @@ cp -Lpv ${PACKAGESDIR}/maven/maven-3.5.4-noarch-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/netbeans/netbeans-8.1.0.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda-9.1.85-${ARCH}-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda_samples-9.1.85-noarch-1.xzm ${MODSDIR}/
-
-# cp -Lpv ${PACKAGESDIR}/chromium/chromium-64.0.3282.119-${ARCH}-1alien.xzm ${MODSDIR}/
-# cp -Lpv ${PACKAGESDIR}/veracrypt/veracrypt-gui-latest-${ARCH}.xzm ${MODSDIR}/
-# cp -Lpv ${PACKAGESDIR}/vivaldi/vivaldi-latest-${ARCH}.xzm ${MODSDIR}/
