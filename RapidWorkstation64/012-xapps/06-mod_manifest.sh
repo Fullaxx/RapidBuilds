89545ce1b3f760543d00ac834ee0091694187cab
Brett Kuskie <fullaxx@gmail.com>
2018-10-08 14:30:47 -0600

    updated all 06-mod_manifest.sh scripts to not error if no *.txz or no *.xzm files exist

--- RapidWorkstation64/012-xapps/06-mod_manifest.sh
+++ RapidWorkstation64/012-xapps/06-mod_manifest.sh
@@ -3,5 +3,14 @@
 source ../spec.sh
 source ./module_spec.sh
 
-mkdir -p ${DIR}/usr/share/rapidlinux/manifests
-ls -1 *.txz > ${DIR}/usr/share/rapidlinux/manifests/${MODNUM}-${MODNAME}.txt
+TXZCOUNT=`find . -name "*.txz" | wc -l`
+if [ "${TXZCOUNT}" != "0" ]; then
+  mkdir -p ${DIR}/usr/share/rapidlinux/manifests
+  ls -1 *.txz >> ${DIR}/usr/share/rapidlinux/manifests/${MODNUM}-${MODNAME}.txt
+fi
+
+XZMCOUNT=`find . -name "*.xzm" | wc -l`
+if [ "${XZMCOUNT}" != "0" ]; then
+  mkdir -p ${DIR}/usr/share/rapidlinux/manifests
+  ls -1 *.xzm >> ${DIR}/usr/share/rapidlinux/manifests/${MODNUM}-${MODNAME}.txt
+fi
