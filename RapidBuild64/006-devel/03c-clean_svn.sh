58ecac46927a5f24c5ea4f87fd4dcfa0e0988211
Brett Kuskie <fullaxx@gmail.com>
2018-10-12 11:19:17 -0600

    updated RapidBuild64/006-devel/03c-clean_svn.sh

--- RapidBuild64/006-devel/03c-clean_svn.sh
+++ RapidBuild64/006-devel/03c-clean_svn.sh
@@ -3,7 +3,10 @@
 source ../spec.sh
 source ./module_spec.sh
 
-# Clean up subversion if we dont have QT
-if [ "${INCSUBVERSION}" == "Y" -a "${INCQT}" != "Y" ]; then
-  rm ${DIR}/usr/${LIBDIR}/libsvn_auth_kwallet-?.*
+# Clean up subversion
+if [ "${INCSUBVERSION}" == "Y" ]; then
+# if we dont have QT
+  if [ "${INCQT}" != "Y" ]; then
+    rm ${DIR}/usr/${LIBDIR}/libsvn_auth_kwallet-?.*
+  fi
 fi
