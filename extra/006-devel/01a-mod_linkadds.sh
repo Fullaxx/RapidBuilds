f89bddc856b480c472cf3c0c30d9faf4a5e5fac3
Brett Kuskie <fullaxx@gmail.com>
2018-11-13 11:59:44 -0700

    added more code examples to extra/006-devel/01a-mod_linkadds.sh

--- extra/006-devel/01a-mod_linkadds.sh
+++ extra/006-devel/01a-mod_linkadds.sh
@@ -12,14 +12,24 @@ if [ "${INCCGDB}" == "Y" ]; then
 fi
 
 if [ "${INCCODEEXAMPLES}" == "Y" ]; then
+  linkpackage "${PACKAGESDIR}/bootstrap-dht/bootstrap-dht-20181113-noarch-bksrc1.xzm"
   linkpackage "${PACKAGESDIR}/bstrlib/bstrlib-20181112-noarch-bksrc1.xzm"
   linkpackage "${PACKAGESDIR}/cJSON/cJSON-20181112-noarch-bksrc1.xzm"
   linkpackage "${PACKAGESDIR}/jsmn/jsmn-20181112-noarch-bksrc1.xzm"
   linkpackage "${PACKAGESDIR}/jsonh/jsonh-20181112-noarch-bksrc1.xzm"
   linkpackage "${PACKAGESDIR}/libLCTHW/libLCTHW-20181112-noarch-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/libbtdht/libbtdht-20181113-noarch-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/libutp/libutp-20181113-noarch-bksrc1.xzm"
   linkpackage "${PACKAGESDIR}/microjson/microjson-20181112-noarch-bksrc1.xzm"
   linkpackage "${PACKAGESDIR}/pdjson/pdjson-20181112-noarch-bksrc1.xzm"
+#  linkpackage "${PACKAGESDIR}/poco/poco-20181113-noarch-bksrc1.xzm"
   linkpackage "${PACKAGESDIR}/tiny-json/tiny-json-20181112-noarch-bksrc1.xzm"
+
+  linkpackage "${PACKAGESDIR}/libtom/libtomcrypt-20181113-noarch-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/libtom/libtomfloat-20181113-noarch-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/libtom/libtommath-20181113-noarch-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/libtom/libtompoly-20181113-noarch-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/libtom/tomsfastmath-20181113-noarch-bksrc1.xzm"
 fi
 
 if [ "${INCVALGRIND}" == "Y" ]; then
