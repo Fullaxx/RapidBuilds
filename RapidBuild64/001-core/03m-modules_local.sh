ddc083d47eb7c34827e02f9a6524401d310691b6
Brett Kuskie <fullaxx@gmail.com>
2018-11-14 10:38:52 -0700

    added ipmi_devintf to 001-core/03m-modules_local.sh

--- RapidBuild64/001-core/03m-modules_local.sh
+++ RapidBuild64/001-core/03m-modules_local.sh
@@ -5,6 +5,9 @@ source ./module_spec.sh
 
 echo "/sbin/modprobe configs" >> ${DIR}/etc/rc.d/rc.modules.local
 echo "/sbin/modprobe tun" >> ${DIR}/etc/rc.d/rc.modules.local
+echo "/sbin/modprobe ipmi_devintf" >> ${DIR}/etc/rc.d/rc.modules.local
+#echo "/sbin/modprobe acpi_ipmi" >> ${DIR}/etc/rc.d/rc.modules.local
+#echo "/sbin/modprobe ipmi_si" >> ${DIR}/etc/rc.d/rc.modules.local
 
 echo >> ${DIR}/etc/rc.d/rc.modules.local
 echo "if /usr/bin/grep flags /proc/cpuinfo | /usr/bin/grep -q vmx; then /sbin/modprobe kvm-intel; fi" >> ${DIR}/etc/rc.d/rc.modules.local
