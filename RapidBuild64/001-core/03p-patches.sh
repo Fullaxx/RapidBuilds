d3a44ef6fc2b7a4c2fdddb85eacf6be1c1360c7a
Brett Kuskie <fullaxx@gmail.com>
2018-11-27 10:49:23 -0700

    updated all startup patching to match slackware-current 181122

--- RapidBuild64/001-core/03p-patches.sh
+++ RapidBuild64/001-core/03p-patches.sh
@@ -3,7 +3,7 @@
 source ../spec.sh
 source ./module_spec.sh
 
-PATCHESDIR="RLBFILES/patches"
+PATCHESDIR="RLBFILES/patches/181122-startup"
 if [ ! -d ${PATCHESDIR}  ]; then
   echo "patches are missing..."
   exit 1
@@ -11,14 +11,14 @@ fi
 
 patch ${DIR}/etc/hosts < ${PATCHESDIR}/localhost6.diff
 
-for DIFF in ${PATCHESDIR}/rc.S-??-*.diff; do patch ${DIR}/etc/rc.d/rc.S < ${DIFF}; done
-
-for DIFF in ${PATCHESDIR}/rc.M-??-*.diff; do patch ${DIR}/etc/rc.d/rc.M < ${DIFF}; done
-
-for DIFF in ${PATCHESDIR}/rc.6-??-*.diff; do patch ${DIR}/etc/rc.d/rc.6 < ${DIFF}; done
-
 patch ${DIR}/etc/rc.d/rc.udev < ${PATCHESDIR}/rc.udev-01-shm.diff
 
 patch ${DIR}/etc/rc.d/rc.modules < ${PATCHESDIR}/rc.modules-01-nodepmod.diff
 
 patch ${DIR}/etc/rc.d/rc.inet2 < ${PATCHESDIR}/rc.inet2.diff
+
+for DIFF in ${PATCHESDIR}/rc.S-??-*.diff; do patch ${DIR}/etc/rc.d/rc.S < ${DIFF}; done
+
+for DIFF in ${PATCHESDIR}/rc.M-??-*.diff; do patch ${DIR}/etc/rc.d/rc.M < ${DIFF}; done
+
+for DIFF in ${PATCHESDIR}/rc.6-??-*.diff; do patch ${DIR}/etc/rc.d/rc.6 < ${DIFF}; done
