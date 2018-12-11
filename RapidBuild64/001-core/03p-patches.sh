b6f4ad56f5117568c53c5208421f61c1240d3c63
Brett Kuskie <fullaxx@gmail.com>
2018-12-11 15:20:33 -0700

    reworked the startup process - now very slackware vanilla

--- RapidBuild64/001-core/03p-patches.sh
+++ RapidBuild64/001-core/03p-patches.sh
@@ -3,7 +3,7 @@
 source ../spec.sh
 source ./module_spec.sh
 
-PATCHESDIR="RLBFILES/patches/181129-startup"
+PATCHESDIR="RLBFILES/patches/181211-startup"
 if [ ! -d ${PATCHESDIR}  ]; then
   echo "patches are missing..."
   exit 1
@@ -11,9 +11,7 @@ fi
 
 patch ${DIR}/etc/hosts < ${PATCHESDIR}/localhost6.diff
 
-patch ${DIR}/etc/rc.d/rc.udev < ${PATCHESDIR}/rc.udev-01-shm.diff
-
-patch ${DIR}/etc/rc.d/rc.modules < ${PATCHESDIR}/rc.modules-01-nodepmod.diff
+patch ${DIR}/etc/rc.d/rc.udev < ${PATCHESDIR}/rc.udev.diff
 
 patch ${DIR}/etc/rc.d/rc.inet2 < ${PATCHESDIR}/rc.inet2.diff
 
