bc8cb87cbe45b1bc35629dafa2b404def85fb6f1
Brett Kuskie <fullaxx@gmail.com>
2018-12-14 06:41:22 -0700

    added initramfs creation to the build process

--- RapidBuild64/baseoutput/04-install_base.sh
+++ RapidBuild64/baseoutput/04-install_base.sh
@@ -13,9 +13,5 @@ if [ ! -d ${BASEDIR} ]; then
   exit 1
 fi
 
-# Copy the specified kernel
-install -D -m 0644 ${KERNELPKGDIR}/bzImage ${BOOTDIR}/vmlinuz
-install -D -m 0644 ${PACKAGESDIR}/initramfs/irfs-20181211.img.lz ${BOOTDIR}/irfs.img
-
 # Copy the modules
 cp -Lpv *.xzm ${BASEDIR}/
--- RapidBuild64/baseoutput/05-stamp_iso.sh
+++ RapidBuild64/baseoutput/05-stamp_iso.sh
@@ -3,6 +3,12 @@
 source ../spec.sh
 source ./module_spec.sh
 
+bail()
+{
+  echo "$1"
+  exit 1
+}
+
 if [ "$TIMESTAMPEDISO" == "1" ]; then
   TIMESTAMP=`date "+%y%m%d-%H%M%S"`
   ISOFILENAME="${PROJNAME}-${TIMESTAMP}.iso"
@@ -10,8 +16,22 @@ else
   ISOFILENAME="${PROJNAME}.iso"
 fi
 
+# Copy the specified kernel and save the initramfs
+install -D -m 0644 ${KERNELPKGDIR}/bzImage ${BOOTDIR}/vmlinuz
+
+if [ ! -d ${IRFSDIR} ]; then
+  echo "${IRFSDIR} never got created!"
+  exit 1
+fi
+
+echo "Saving initramfs ..."
+( set -e; cd ${IRFSDIR} && find . | cpio -H newc -o | lzma > ${BOOTDIR}/irfs.img )
+
 # Set Permissions
 chown -R root.root ${RLDIR}
 
 # Build the ISO
-( cd ${RLDIR} && ./make_iso.sh ${ISOOUTPUTDIR}/${ISOFILENAME} && echo "${ISOOUTPUTDIR}/${ISOFILENAME} created" || echo "ISO CREATION FAILED!" )
+cd ${RLDIR}
+./make_iso.sh ${ISOOUTPUTDIR}/${ISOFILENAME} || bail "ISO CREATION FAILED!"
+
+echo "${ISOOUTPUTDIR}/${ISOFILENAME} created"
