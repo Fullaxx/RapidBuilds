4feb6500a1a35a89c0d600c7a4aee1bfb4a60a87
Brett Kuskie <fullaxx@gmail.com>
2018-12-31 06:28:53 -0700

    Rewrote RapidBuild64/002-utils/03c-clean_groff.sh to handle multiple doc dirs

--- RapidBuild64/002-utils/03c-clean_groff.sh
+++ RapidBuild64/002-utils/03c-clean_groff.sh
@@ -3,7 +3,16 @@
 source ../spec.sh
 source ./module_spec.sh
 
-# clean up groff if we don't have X
-if [ "${INCGROFF}" == "Y" -a "${INCMOD010XORG}" != "Y" ]; then
-  rm ${DIR}/usr/bin/{gxditview,xtotroff}
+if [ "${INCGROFF}" == "Y" ]; then
+# Clean up Groff if we don't have X
+  if [ "${INCMOD010XORG}" != "Y" ]; then
+    rm ${DIR}/usr/bin/{gxditview,xtotroff}
+  fi
+
+# Fix Groff Documentation
+  GROFFVER=`ls -1 groff-*.txz | cut -d- -f2`
+  if [ -d ${DIR}/usr/share/doc/groff-${GROFFVER} ] && [ -d ${DIR}/usr/doc/groff-${GROFFVER} ]; then
+    mv ${DIR}/usr/share/doc/groff-${GROFFVER}/* ${DIR}/usr/doc/groff-${GROFFVER}/
+    rmdir ${DIR}/usr/share/doc/groff-${GROFFVER}
+  fi
 fi
