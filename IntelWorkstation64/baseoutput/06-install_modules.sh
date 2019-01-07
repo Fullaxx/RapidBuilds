6dfef8022f1c0eba5d5a48c036b9ae2cebb50b05
Brett Kuskie <fullaxx@gmail.com>
2019-01-07 15:51:30 -0700

    Upgtaded to Netbeans 10.0

--- IntelWorkstation64/baseoutput/06-install_modules.sh
+++ IntelWorkstation64/baseoutput/06-install_modules.sh
@@ -32,4 +32,4 @@ cp -Lpv ${PACKAGESDIR}/docker/docker-18.09.0-${ARCH}-4.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/java8/jdkdocs-8u191-noarch-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/java8/jdk-8u191-${ARCH}-1.xzm ${MODSDIR}/
 cp -Lpv ${PACKAGESDIR}/maven/maven-3.5.4-noarch-2.xzm ${MODSDIR}/
-cp -Lpv ${PACKAGESDIR}/netbeans/netbeans-9.0-noarch-1.xzm ${MODSDIR}/
+cp -Lpv ${PACKAGESDIR}/netbeans/netbeans-10.0-noarch-1.xzm ${MODSDIR}/
