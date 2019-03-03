372e2d2526d7cb1b6c6eb70b364d829fda700164
Brett Kuskie <fullaxx@gmail.com>
2019-03-03 06:37:15 -0700

    Updated to nng-1.1.1 and mbedtls-2.16.0

--- extra/005-server/01a-mod_linkadds.sh
+++ extra/005-server/01a-mod_linkadds.sh
@@ -24,8 +24,8 @@ if [ "${INCNGINX}" == "Y" ]; then
 fi
 
 if [ "${INCNNG}" == "Y" ]; then
-  linkpackage "${PACKAGESDIR}/nng/nng-1.0.1-${ARCH}-bksrc1.xzm"
-  linkpackage "${PACKAGESDIR}/mbedtls/mbedtls-2.13.0-${ARCH}-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/mbedtls/mbedtls-2.16.0-${ARCH}-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/nng/nng-1.1.1-${ARCH}-bksrc1.xzm"
 fi
 
 if [ "${INCNODEZEROTEN}" == "Y" ]; then
