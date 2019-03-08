420dd502d8526934136c59ce76704e2802ba4e98
Brett Kuskie <fullaxx@gmail.com>
2019-03-08 08:17:34 -0700

    Grub config was getting the wrong partition number (p2) during install if installing on nvme drive, make it be 2 instead of p2

--- RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rli_02_copy.sh
+++ RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rli_02_copy.sh
@@ -40,6 +40,11 @@ if [ -d "${MNTDIR}"/boot/grub/locale ]; then
   rm -rf "${MNTDIR}"/boot/grub/locale
 fi
 
+# for NVMe devices
+if echo "${RLPARTNUM}" | grep -q 'p' ; then
+  RLPARTNUM=`echo "${RLPARTNUM}" | cut -dp -f2`
+fi
+
 sed -e "s/hd0,x/hd0,${RLPARTNUM}/" -i "${MNTDIR}"/boot/grub/grub.cfg
 # export NOW=`date "+%Y%m%d%H%M%S"`
 # echo -n $NOW | sha1sum
