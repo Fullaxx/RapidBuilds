30f06abd4a1493e7deeeea7e8ab0ec48df92e555
Brett Kuskie <fullaxx@gmail.com>
2019-03-26 07:23:16 -0600

    Updated to chromium-73.0.3683.75 and vivaldi-codecs-ffmpeg-extra-72.0.3626.121

--- IntelLaptop64/baseoutput/06-install_modules.sh
+++ IntelLaptop64/baseoutput/06-install_modules.sh
@@ -22,10 +22,10 @@ fi
 # Copy any extra modules that don't need RDL
 verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.26-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/LibreOffice/LibreOffice-5.4.7-${ARCH}-1.xzm ${MODSDIR}/
-verbosecopymodule ${PACKAGESDIR}/chromium/chromium-72.0.3626.81-${ARCH}-1alien.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/chromium/chromium-73.0.3683.75-${ARCH}-1alien.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/dbwalls/dbwalls-1920-130207.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/docker/docker-18.09.3-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/java8/jre-8u191-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/veracrypt/veracrypt-console-1.23-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/vivaldi/vivaldi-stable-2.3.1440.61-1.${ARCH}.xzm ${MODSDIR}/
-verbosecopymodule ${PACKAGESDIR}/vivaldi-codecs-ffmpeg-extra/vivaldi-codecs-ffmpeg-extra-71.0.3578.98-${ARCH}-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/vivaldi-codecs-ffmpeg-extra/vivaldi-codecs-ffmpeg-extra-72.0.3626.121-${ARCH}-1.xzm ${MODSDIR}/
