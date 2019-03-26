bfe88546591d9c1b6ed6fcff0a7b5e7ada418e5b
Brett Kuskie <fullaxx@gmail.com>
2019-03-26 06:02:48 -0600

    Update /usr/bin/rl_mnt_cdrom.sh with error checking

--- RapidBuild64/002-utils/RLBFILES/rootcopy/usr/bin/rl_mnt_cdrom.sh
+++ RapidBuild64/002-utils/RLBFILES/rootcopy/usr/bin/rl_mnt_cdrom.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+if [ ! -b /dev/sr0 ]; then
+  echo "/dev/sr0 does not exist!"
+  sleep 9
+  exit 1
+fi
+
+if mount | grep ' on /mnt/cdrom' ; then
+  echo "Something is already mounted on /mnt/cdrom"
+  sleep 9
+  exit 1
+fi
+
 while ! grep -wq "/dev/sr0 /mnt/cdrom" /etc/mtab; do
-  if [ x"$1" != "x" ]; then sleep $1; fi
-  mount /dev/cdrom
+  if [ x"$1" != "x" ]; then sleep "$1"; fi
+  mount /dev/sr0 /mnt/cdrom
 done
