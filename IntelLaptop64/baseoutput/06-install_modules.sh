4fe4e0537cc31df074b2b1ebf53e425cde7ede11
Brett Kuskie <fullaxx@gmail.com>
2019-05-29 04:21:40 -0600

    Update to VirtualBox-5.2.30

--- IntelLaptop64/baseoutput/06-install_modules.sh
+++ IntelLaptop64/baseoutput/06-install_modules.sh
@@ -20,7 +20,7 @@ if [ "${INCVSCODE}" == "Y" ]; then
 fi
 
 # Copy any extra modules that don't need RDL
-verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.26-${ARCH}-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.30-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/LibreOffice/LibreOffice-5.4.7-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/archipelago/archipelago-3.2.1-x86_64-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/chromium/chromium-73.0.3683.75-${ARCH}-1alien.xzm ${MODSDIR}/
