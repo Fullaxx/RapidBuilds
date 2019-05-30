0795b90923a0f43c80b576e475d5b6278515d5aa
Brett Kuskie <fullaxx@gmail.com>
2019-05-30 07:17:48 -0600

    Update to rapidconfig-rapidworkstation-20190530

--- CudaWorkstation64/baseoutput/06-install_modules.sh
+++ CudaWorkstation64/baseoutput/06-install_modules.sh
@@ -43,4 +43,4 @@ verbosecopymodule ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda-10.0.130-${ARCH}-1.xzm
 verbosecopymodule ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda_samples-10.0.130-noarch-1.xzm ${MODSDIR}/
 
 # Desktop Configuration
-verbosecopymodule ${PACKAGESDIR}/rapidconfigs/rapidconfig-rapidworkstation-20190409-noarch-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/rapidconfigs/rapidconfig-rapidworkstation-20190530-noarch-1.xzm ${MODSDIR}/
--- IntelWorkstation64/baseoutput/06-install_modules.sh
+++ IntelWorkstation64/baseoutput/06-install_modules.sh
@@ -38,4 +38,4 @@ verbosecopymodule ${PACKAGESDIR}/maven/maven-3.5.4-noarch-2.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/netbeans/netbeans-8.2.0.xzm ${MODSDIR}/
 
 # Desktop Configuration
-verbosecopymodule ${PACKAGESDIR}/rapidconfigs/rapidconfig-rapidworkstation-20190409-noarch-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/rapidconfigs/rapidconfig-rapidworkstation-20190530-noarch-1.xzm ${MODSDIR}/
