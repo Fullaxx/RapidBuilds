bd10d0622cb5e5d48e5011774a9ae3dedd37d754
Brett Kuskie <fullaxx@gmail.com>
2019-08-02 07:07:39 -0600

    Upgrade to VirtualBox-5.2.32

--- CudaWorkstation64/baseoutput/06-install_modules.sh
+++ CudaWorkstation64/baseoutput/06-install_modules.sh
@@ -26,7 +26,7 @@ if [ "${INCVSCODE}" == "Y" ]; then
 fi
 
 # Copy any extra modules that don't need RDL
-verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.30-${ARCH}-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.32-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/LibreOffice/LibreOffice-5.4.7-${ARCH}-1.xzm ${MODSDIR}/
 # verbosecopymodule ${PACKAGESDIR}/archipelago/archipelago-3.2.1-x86_64-1.xzm ${MODSDIR}/
 # verbosecopymodule ${PACKAGESDIR}/cutter/cutter-1.8.0-x86_64-1.xzm ${MODSDIR}/
--- IntelLaptop64/baseoutput/06-install_modules.sh
+++ IntelLaptop64/baseoutput/06-install_modules.sh
@@ -20,7 +20,7 @@ if [ "${INCVSCODE}" == "Y" ]; then
 fi
 
 # Copy any extra modules that don't need RDL
-verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.30-${ARCH}-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-5.2.32-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/LibreOffice/LibreOffice-5.4.7-${ARCH}-1.xzm ${MODSDIR}/
 # verbosecopymodule ${PACKAGESDIR}/archipelago/archipelago-3.2.1-x86_64-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/chromium/chromium-75.0.3770.142-${ARCH}-1alien.xzm ${MODSDIR}/
