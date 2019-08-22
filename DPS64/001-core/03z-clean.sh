18ca3227b6bd434de3ff39f5e98a54030a1e32dd
Brett Kuskie <fullaxx@gmail.com>
2019-08-22 04:40:41 -0600

    DPS64: Update 001-core/03z-clean.sh to clean tc when netfilter is absent

--- DPS64/001-core/03z-clean.sh
+++ DPS64/001-core/03z-clean.sh
@@ -3,9 +3,17 @@
 source ../spec.sh
 source ./module_spec.sh
 
+# If we don't have netfilter, we can't do traffic control
+if [ "${INCNETFILTER}" != "Y" ]; then
+  rm ${DIR}/sbin/tc ${DIR}/usr/sbin/tc
+#  rm ${DIR}/usr/${LIBDIR}/tc/{m_xt.so,m_ipt.so}
+  rm -r ${DIR}/usr/${LIBDIR}/tc
+  rm ${DIR}/usr/share/bash-completion/completions/tc
+fi
+
 # Don't need these for a Docker base image
-rm -f ${DIR}/usr/$LIBDIR/libidn.so.*
-rm -f ${DIR}/usr/$LIBDIR/{libcups,libcupsimage}.*
-rm -f ${DIR}/usr/$LIBDIR/{libjpeg,libturbojpeg,libpng14,libpng16,libtiff,libtiffxx}.*
+rm -f ${DIR}/usr/${LIBDIR}/libidn.so.*
+rm -f ${DIR}/usr/${LIBDIR}/{libcups,libcupsimage}.*
+rm -f ${DIR}/usr/${LIBDIR}/{libjpeg,libturbojpeg,libpng14,libpng16,libtiff,libtiffxx}.*
 
 rm -rf ${DIR}/opt/refind
