52d7a6a2940c80fdfd1acc783fc1a1fe5f6a6a61
Brett Kuskie <fullaxx@gmail.com>
2020-03-12 06:53:28 -0600

    Add DPS64/baseoutput/06-install_modules.sh

---
+++ DPS64/baseoutput/06-install_modules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ../spec.sh
+source ./module_spec.sh
+
+if [ ! -d ${MODSDIR} ]; then
+  echo "${MODSDIR} deos not exist!"
+  exit 1
+fi
+
+# Copy any extra modules that don't need RDL
+# DPS64 doesnt need curlftpfs
+# verbosecopymodule ${PACKAGESDIR}/curlftpfs/curlftpfs-0.9.2-${ARCH}-bksrc1.xzm ${MODSDIR}/
