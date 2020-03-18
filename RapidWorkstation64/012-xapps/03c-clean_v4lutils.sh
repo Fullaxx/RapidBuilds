78fcb6bdfa59c1e32d30005a9c6ed17b7952b2f9
Brett Kuskie <fullaxx@gmail.com>
2020-03-18 15:11:36 -0600

    Update 03c-clean_v4lutils.sh

--- RapidWorkstation64/012-xapps/03c-clean_v4lutils.sh
+++ RapidWorkstation64/012-xapps/03c-clean_v4lutils.sh
@@ -3,11 +3,8 @@
 source ../spec.sh
 source ./module_spec.sh
 
-# /usr/bin/qv4l2 is gone as of ??
-# Fri Mar 29 18:24:36 UTC 2019
-# l/v4l-utils-1.16.5-x86_64-1.txz:  Upgraded.
-
-# Clean up v4l-utils, if we dont have QT
-#if [ "${INCVFOURLUTILS}" == "Y" ] && [ "${INCQT}" != "Y" ]; then
-#  rm ${DIR}/usr/bin/qv4l2
-#fi
+# Clean up v4l-utils, if we dont have QT5
+if [ "${INCVFOURLUTILS}" == "Y" ] && [ "${INCQTFIVE}" != "Y" ]; then
+  rm ${DIR}/usr/bin/qv4l2
+  rm ${DIR}/usr/bin/qvidcap
+fi
