339361f8e2370257e5437685056b572d6db2c827
Brett Kuskie <fullaxx@gmail.com>
2020-04-04 07:16:30 -0600

    Update to vivaldi-2.11.1811.52

--- IntelLaptop64/baseoutput/06-install_modules.sh
+++ IntelLaptop64/baseoutput/06-install_modules.sh
@@ -30,4 +30,4 @@ verbosecopymodule ${PACKAGESDIR}/docker/docker-19.03.7-${ARCH}-1.xzm ${MODSDIR}/
 # verbosecopymodule ${PACKAGESDIR}/hyper/hyper-2.1.2-x86_64-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/java8/jre-8u191-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/veracrypt/veracrypt-console-1.24.4-${ARCH}-1.xzm ${MODSDIR}/
-verbosecopymodule ${PACKAGESDIR}/vivaldi/vivaldi-2.11.1811.49-${ARCH}-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/vivaldi/vivaldi-2.11.1811.52-${ARCH}-1.xzm ${MODSDIR}/
