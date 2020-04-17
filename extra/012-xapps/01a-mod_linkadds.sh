7c63d1710a63bad489afe8fbbe65291c7f9858d0
Brett Kuskie <fullaxx@gmail.com>
2020-04-17 06:48:33 -0600

    Update to wireshark-3.0.10

--- extra/012-xapps/01a-mod_linkadds.sh
+++ extra/012-xapps/01a-mod_linkadds.sh
@@ -110,7 +110,8 @@ fi
 
 if [ "${INCWIRESHARK}" == "Y" ]; then
   linkpackage "${PACKAGESDIR}/portaudio/portaudio-v19-${ARCH}-bksrc1.xzm"
-  linkpackage "${PACKAGESDIR}/wireshark/wireshark-2.6.14-GTK2-${ARCH}-bksrc1.xzm"
+#  linkpackage "${PACKAGESDIR}/wireshark/wireshark-2.6.14-GTK2-${ARCH}-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/wireshark/wireshark-3.0.10-${ARCH}-bksrc1.xzm"
 fi
 
 if [ "${INCXFE}" == "Y" ]; then
