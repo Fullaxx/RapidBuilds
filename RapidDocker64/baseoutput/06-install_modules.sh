9eee698ed03606a88be10f9cfdf64d62f17b508a
Brett Kuskie <fullaxx@gmail.com>
2020-04-20 16:32:52 -0600

    Update to nvidia-440.82

--- RapidDocker64/baseoutput/06-install_modules.sh
+++ RapidDocker64/baseoutput/06-install_modules.sh
@@ -12,7 +12,7 @@ verbosecopymodule ${PACKAGESDIR}/docker/docker-19.03.8-${ARCH}-1.xzm ${MODSDIR}/
 
 ### export ENABLE_NVIDIA_FOR_DOCKER="1" for nvidia-container-runtime support ###
 if [ "${ENABLE_NVIDIA_FOR_DOCKER}" == "1" ]; then
-  verbosecopymodule ${PACKAGESDIR}/nvidia_driver/011-nvidia-440.64-${KERNVERS}.xzm ${BASEDIR}/
+  verbosecopymodule ${PACKAGESDIR}/nvidia_driver/011-nvidia-440.82-${KERNVERS}.xzm ${BASEDIR}/
   verbosecopymodule ${PACKAGESDIR}/libseccomp/libseccomp-2.4.3-${ARCH}-bksrc1.xzm ${MODSDIR}/
   verbosecopymodule ${PACKAGESDIR}/nvidia-container-runtime/nvidia-container-runtime-3.1.4-${ARCH}-1.xzm ${MODSDIR}/
 fi
