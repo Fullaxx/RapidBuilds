eb94e56ac2855a5a5e3ab48632099b0d17dc59c9
Brett Kuskie <fullaxx@gmail.com>
2020-08-06 15:35:58 -0600

    Add linmaxminddb as a dependency of Wireshark

--- RDL/apps/012-wireshark.sh
+++ RDL/apps/012-wireshark.sh
@@ -1,4 +1,5 @@
 if [ "${INCWIRESHARK}" == "Y" ]; then
+  INCLIBMAXMINDDB="Y"
   INCLZFOUR="Y"
   INCPORTAUDIO="Y"
 # PORTAUDIO is brought in with INCWIRESHARK at extra/012-apps/01a-mod_linkadds.sh at the moment
