149c7c2c0a8d4a3fe4772b596854b41354699e38
Brett Kuskie <fullaxx@gmail.com>
2020-08-17 03:00:03 -0600

    Add RapidBuild64/001-core/RLBFILES/patches/200515-startup

--- RapidBuild64/001-core/03p-patches.sh
+++ RapidBuild64/001-core/03p-patches.sh
@@ -3,7 +3,7 @@
 source ../spec.sh
 source ./module_spec.sh
 
-PATCHESDIR="RLBFILES/patches/191227-startup"
+PATCHESDIR="RLBFILES/patches/200515-startup"
 if [ ! -d ${PATCHESDIR}  ]; then
   echo "patches are missing..."
   exit 1
