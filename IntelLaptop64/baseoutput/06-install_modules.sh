3b3fca565d846ef6f30253fda6784b2ffc7d0751
Brett Kuskie <fullaxx@gmail.com>
2020-08-21 10:56:40 -0600

    Update to chrome-84.0.4147.135

--- IntelLaptop64/baseoutput/06-install_modules.sh
+++ IntelLaptop64/baseoutput/06-install_modules.sh
@@ -11,7 +11,7 @@ fi
 
 # Chrome needs RDL
 if [ "${INCCHROME}" == "Y" ]; then
-  verbosecopymodule ${PACKAGESDIR}/chrome/chrome-84.0.4147.125-x86_64-1.xzm ${MODSDIR}/
+  verbosecopymodule ${PACKAGESDIR}/chrome/chrome-84.0.4147.135-x86_64-1.xzm ${MODSDIR}/
 fi
 
 # VSCode needs RDL
