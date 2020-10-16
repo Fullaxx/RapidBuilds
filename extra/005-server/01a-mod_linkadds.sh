24ef4676fc8c17e58089181b7995466072ae1fdd
Brett Kuskie <fullaxx@gmail.com>
2020-10-16 07:08:52 -0600

    Update to libvirt-6.8.0

--- extra/005-server/01a-mod_linkadds.sh
+++ extra/005-server/01a-mod_linkadds.sh
@@ -12,7 +12,7 @@ if [ "${INCLIBMICROHTTPD}" == "Y" ]; then
 fi
 
 if [ "${INCLIBVIRT}" == "Y" ]; then
-  linkpackage "${PACKAGESDIR}/libvirt/libvirt-6.6.0-${ARCH}-bksrc1.xzm"
+  linkpackage "${PACKAGESDIR}/libvirt/libvirt-6.8.0-${ARCH}-bksrc1.xzm"
 fi
 
 if [ "${INCLIBWEBSOCKETS}" == "Y" ]; then
