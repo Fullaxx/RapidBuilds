a143d490898a2a4236f8d5140476e40bb88e944e
Brett Kuskie <fullaxx@gmail.com>
2020-11-03 11:58:56 -0700

    Update to vscode-1.49.3

--- IntelLaptop64/baseoutput/06-install_modules.sh
+++ IntelLaptop64/baseoutput/06-install_modules.sh
@@ -16,7 +16,7 @@ fi
 
 # VSCode needs RDL
 if [ "${INCVSCODE}" == "Y" ]; then
-  verbosecopymodule ${PACKAGESDIR}/vscode/vscode-1.48.2-${ARCH}-1.xzm ${MODSDIR}/
+  verbosecopymodule ${PACKAGESDIR}/vscode/vscode-1.49.3-${ARCH}-1.xzm ${MODSDIR}/
 fi
 
 # LibreOffice needs avahi-libs
