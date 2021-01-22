1b035fcd828038147829a4c9c2da18c7c82bdf35
Brett Kuskie <fullaxx@gmail.com>
2021-01-22 07:01:33 -0700

    Remove libqtexttospeech_speechd.so from qt5

--- RapidWorkstation64/012-xapps/03c-clean_qt.sh
+++ RapidWorkstation64/012-xapps/03c-clean_qt.sh
@@ -5,22 +5,20 @@ source ./module_spec.sh
 
 # Clean up QT
 if [ "${INCQT}" == "Y" ]; then
-
-if [ "${INCMARIADB}" != "Y" ]; then
-  rm ${DIR}/usr/${LIBDIR}/qt/plugins/sqldrivers/libqsqlmysql.so
-fi
-
-if [ "${INCLIBIODBC}" != "Y" ]; then
-  rm ${DIR}/usr/${LIBDIR}/qt/plugins/sqldrivers/libqsqlodbc.so
-fi
-
+  if [ "${INCMARIADB}" != "Y" ]; then
+    rm ${DIR}/usr/${LIBDIR}/qt/plugins/sqldrivers/libqsqlmysql.so
+  fi
+  if [ "${INCLIBIODBC}" != "Y" ]; then
+    rm ${DIR}/usr/${LIBDIR}/qt/plugins/sqldrivers/libqsqlodbc.so
+  fi
 fi
 
 # Clean up QT5
 if [ "${INCQTFIVE}" == "Y" ] && [ "${INCSLACKQTFIVE}" == "Y" ]; then
+  rm ${DIR}/usr/${LIBDIR}/qt5/plugins/texttospeech/libqtexttospeech_speechd.so
+  rmdir ${DIR}/usr/${LIBDIR}/qt5/plugins/texttospeech
 
-if [ "${INCMARIADB}" != "Y" ]; then
-  rm ${DIR}/usr/${LIBDIR}/qt5/plugins/sqldrivers/libqsqlmysql.so
-fi
-
+  if [ "${INCMARIADB}" != "Y" ]; then
+    rm ${DIR}/usr/${LIBDIR}/qt5/plugins/sqldrivers/libqsqlmysql.so
+  fi
 fi
