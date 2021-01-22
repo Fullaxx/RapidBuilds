f86b7bc2cef4c3599aa6515996b5871cfc88a58d
Brett Kuskie <fullaxx@gmail.com>
2021-01-22 07:02:17 -0700

    Update RapidWorkstation64/012-xapps/03c-clean_poppler.sh

--- RapidWorkstation64/012-xapps/03c-clean_poppler.sh
+++ RapidWorkstation64/012-xapps/03c-clean_poppler.sh
@@ -3,11 +3,6 @@
 source ../spec.sh
 source ./module_spec.sh
 
-# Clean up libpoppler, if we dont have QT4
-#if [ "${INCPOPPLER}" == "Y" ] && [ "${INCQT}" != "Y" ]; then
-#  rm ${DIR}/usr/${LIBDIR}/libpoppler-qt4*
-#fi
-
 # Clean up libpoppler, if we dont have QT5
 if [ "${INCPOPPLER}" == "Y" ] && [ "${INCQTFIVE}" != "Y" ]; then
   rm ${DIR}/usr/${LIBDIR}/libpoppler-qt5*
