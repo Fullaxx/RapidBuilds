e92bbff714ce37d544376fcce12a5d52a8ba3726
Brett Kuskie <fullaxx@gmail.com>
2021-08-04 16:17:09 -0600

    Update to vscode-1.57.1

--- IntelWorkstation64/baseoutput/06-install_modules.sh
+++ IntelWorkstation64/baseoutput/06-install_modules.sh
@@ -20,7 +20,7 @@ fi
 
 # VSCode needs RDL
 if [ "${INCVSCODE}" == "Y" ]; then
-  verbosecopymodule ${PACKAGESDIR}/vscode/vscode-1.55.2-${ARCH}-1.xzm ${MODSDIR}/
+  verbosecopymodule ${PACKAGESDIR}/vscode/vscode-1.57.1-${ARCH}-1.xzm ${MODSDIR}/
 fi
 
 # LibreOffice needs avahi-libs
