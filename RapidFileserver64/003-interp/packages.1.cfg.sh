21e7c817b943e397396b3e1b8129606d1dd3b503
Brett Kuskie <fullaxx@gmail.com>
2021-09-17 16:30:40 -0600

    Add perl to RapidFileserver64/003-interp

--- RapidFileserver64/003-interp/packages.1.cfg.sh
+++ RapidFileserver64/003-interp/packages.1.cfg.sh
@@ -1,1 +1,17 @@
-../basis/003-interp/packages.1.cfg.sh
+# 003-interp options
+if [ "${INCMOD003INTERP}" == "Y" ]; then
+  INCPERL="Y"
+  INCPYTHON="Y"
+  INCPYTHONTWO="N"
+  INCRUBY="N"
+  INCLUA="N"
+  INCTCL="N"
+
+  INCPYTHONSETUPTOOLS="N"
+
+# n family of packages
+  INCPSSH="N"
+
+# just about everything needs db48
+  INCDB48="Y"
+fi
--- RapidFileserver64/003-interp/packages.2.cfg.sh
+++
@@ -1,6 +0,0 @@
-# 003-interp options
-if [ "${INCMOD003INTERP}" == "Y" ]; then
-  INCPERL="N"
-
-  INCPYTHONSETUPTOOLS="N"
-fi
