dd332e0fd0e0b6f2cad0c27dc89233da242552d7
Brett Kuskie <fullaxx@gmail.com>
2021-10-05 17:30:12 -0600

    Update to vivaldi-4.1.2369.21

--- CudaWorkstation64/baseoutput/06-install_modules.sh
+++ CudaWorkstation64/baseoutput/06-install_modules.sh
@@ -46,7 +46,7 @@ verbosecopymodule ${PACKAGESDIR}/docker/docker-19.03.15-${ARCH}-1.xzm ${MODSDIR}
 # verbosecopymodule ${PACKAGESDIR}/netbeans/netbeans-8.2.0.xzm ${MODSDIR}/
 #verbosecopymodule ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda-10.1.243-${ARCH}-1.xzm ${MODSDIR}/
 #verbosecopymodule ${PACKAGESDIR}/nvidia_cuda_toolkit/cuda_samples-10.1.243-noarch-1.xzm ${MODSDIR}/
-verbosecopymodule ${PACKAGESDIR}/vivaldi/vivaldi-4.1.2369.18-${ARCH}-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/vivaldi/vivaldi-4.1.2369.21-${ARCH}-1.xzm ${MODSDIR}/
 
 # Desktop Configuration
 verbosecopymodule ${PACKAGESDIR}/rapidconfigs/rapidconfig-rapidworkstation-20200914-noarch-1.xzm ${MODSDIR}/
