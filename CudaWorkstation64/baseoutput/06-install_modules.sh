a217e5f07b36745938f152086a928ff526e22aa7
Brett Kuskie <fullaxx@gmail.com>
2021-10-05 17:01:38 -0600

    vscode-1.59.1

--- CudaWorkstation64/baseoutput/06-install_modules.sh
+++ CudaWorkstation64/baseoutput/06-install_modules.sh
@@ -28,7 +28,7 @@ fi
 
 # VSCode needs RDL
 if [ "${INCVSCODE}" == "Y" ]; then
-  verbosecopymodule ${PACKAGESDIR}/vscode/vscode-1.57.1-${ARCH}-1.xzm ${MODSDIR}/
+  verbosecopymodule ${PACKAGESDIR}/vscode/vscode-1.59.1-${ARCH}-1.xzm ${MODSDIR}/
 fi
 
 # Copy any extra modules that don't need RDL
