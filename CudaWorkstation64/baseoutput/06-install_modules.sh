d24153e4aef2ee325e4e161fab79992b9cb88c7d
Brett Kuskie <fullaxx@gmail.com>
2022-03-04 05:50:28 -0700

    Update to chrome-98.0.4758.102

--- CudaWorkstation64/baseoutput/06-install_modules.sh
+++ CudaWorkstation64/baseoutput/06-install_modules.sh
@@ -17,7 +17,7 @@ verbosecopymodule ${PACKAGESDIR}/nvidia_driver/011-nvidia-470.63.01-${KERNVERS}.
 
 # Chrome needs RDL
 if [ "${INCCHROME}" == "Y" ]; then
-  verbosecopymodule ${PACKAGESDIR}/chrome/chrome-95.0.4638.69-x86_64-1.xzm ${MODSDIR}/
+  verbosecopymodule ${PACKAGESDIR}/chrome/chrome-98.0.4758.102-x86_64-1.xzm ${MODSDIR}/
 fi
 
 # LibreOffice needs RDL
