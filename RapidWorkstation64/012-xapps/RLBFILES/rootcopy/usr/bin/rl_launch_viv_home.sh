b89b81c3cea0cc5f6ecb31a0afd1953d30db70e1
Brett Kuskie <fullaxx@gmail.com>
2022-03-20 06:52:49 -0600

    Update vivaldi launch scripts

--- RapidWorkstation64/012-xapps/RLBFILES/rootcopy/usr/bin/rl_launch_viv_home.sh
+++ RapidWorkstation64/012-xapps/RLBFILES/rootcopy/usr/bin/rl_launch_viv_home.sh
@@ -7,3 +7,18 @@ unset NOSANDBOX
 if [ `id -u` == "0" ]; then NOSANDBOX="--no-sandbox"; fi
 
 vivaldi-stable ${NOSANDBOX} --user-data-dir=${BROWSERDIR} 2>${BROWSERDIR}/err.log
+
+if grep 'Proprietary media' ${BROWSERDIR}/err.log; then
+  while [ ! -f ~/.local/lib/vivaldi/media-codecs-*/libffmpeg.so ]; do
+    echo -n "."
+    sleep 0.1
+  done
+  echo -n "."
+  sleep 1
+  echo -n "."
+  sleep 1
+  echo -n "."
+  sleep 1
+  rm ${BROWSERDIR}/err.log
+  $0
+fi
