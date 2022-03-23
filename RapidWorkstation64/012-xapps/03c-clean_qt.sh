15df0bd1d1eb5d249fbc1493758f3be1263bb23e
Brett Kuskie <fullaxx@gmail.com>
2022-03-23 15:31:02 -0600

    Update to wireshark-3.4.12

--- RapidWorkstation64/012-xapps/03c-clean_qt.sh
+++ RapidWorkstation64/012-xapps/03c-clean_qt.sh
@@ -7,6 +7,10 @@ source ./module_spec.sh
 if [ "${INCQTFIVE}" == "Y" ]; then
 # libgstcamerabin needs gst-plugins-bad-free
   rm ${DIR}/usr/${LIBDIR}/qt5/plugins/mediaservice/libgstcamerabin.so
+# During a wireshark build, it was discovered that this file needs to exist
+# Evidently the QT make system will barf if it can't find all its limbs
+# despite the fact that it's completely unnecessary
+  touch ${DIR}/usr/${LIBDIR}/qt5/plugins/mediaservice/libgstcamerabin.so
 
   rm ${DIR}/usr/${LIBDIR}/qt5/plugins/texttospeech/libqtexttospeech_speechd.so
   rmdir ${DIR}/usr/${LIBDIR}/qt5/plugins/texttospeech
