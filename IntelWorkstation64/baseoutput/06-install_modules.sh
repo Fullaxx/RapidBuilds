aacf681ad3fbff7c5d25e3d8fd37888897c30042
Brett Kuskie <fullaxx@gmail.com>
2022-06-21 14:19:51 -0600

    Update to vscode-1.67.2

--- IntelWorkstation64/baseoutput/06-install_modules.sh
+++ IntelWorkstation64/baseoutput/06-install_modules.sh
@@ -26,7 +26,7 @@ fi
 
 # VSCode needs RDL
 if [ "${INCVSCODE}" == "Y" ]; then
-  verbosecopymodule ${PACKAGESDIR}/vscode/vscode-1.65.2-${ARCH}-1.xzm ${MODSDIR}/
+  verbosecopymodule ${PACKAGESDIR}/vscode/vscode-1.67.2-${ARCH}-1.xzm ${MODSDIR}/
 fi
 
 # Copy any extra modules that don't need RDL
