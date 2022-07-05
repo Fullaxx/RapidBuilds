6b2ff6f1848110297e3879dceb75417a4b7f414b
Brett Kuskie <fullaxx@gmail.com>
2022-07-05 15:39:21 -0600

    Update rl_change_ntp_server.sh to not modify in place

--- RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rl_change_ntp_server.sh
+++ RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rl_change_ntp_server.sh
@@ -5,15 +5,19 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Stop the Ntpd daemon
+# Stop the ntp daemon
 /etc/rc.d/rc.ntpd stop
 
 # delete any references to pool.ntp.org
-sed -e '/0.pool.ntp.org/d' -i /etc/hosts
+# dont modify in place to support docker volume mount of /etc/hosts
+TEMP=`mktemp`
+cat /etc/hosts | sed -e '/0.pool.ntp.org/d' >${TEMP}
+cat ${TEMP} >/etc/hosts
+rm ${TEMP}
 
 # add our reference to pool.ntp.org
 echo "Setting NTP server to $1..."
-echo "$1 0.pool.ntp.org gps" >> /etc/hosts
+echo "$1 0.pool.ntp.org gps" >>/etc/hosts
 
-# Start the Ntp daemon
+# Start the ntp daemon
 /etc/rc.d/rc.ntpd start
