0b436f4308d446c01ef84b11d9e202abc6dcf6fb
Brett Kuskie <fullaxx@gmail.com>
2022-11-03 15:30:04 -0600

    Update rl_list_netlinks.sh

--- RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rl_list_netlinks.sh
+++ RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rl_list_netlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat /proc/net/dev | grep -Ev "Inter|face" | cut -d: -f1 | grep -v vboxnet | xargs -n1 ethtool | grep -E "Settings|Speed|Link"
+cat /proc/net/dev | grep -Ev "Inter|face" | cut -d: -f1 | grep -v vboxnet | xargs -n1 ethtool | grep -E "Settings|Speed|Link d"
 
 # ifconfig -s | grep -v Iface | cut -f1 '-d '
 
