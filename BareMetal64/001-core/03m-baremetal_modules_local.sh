e263ab6e85b22df0e7c09d25c1d79016da88eab9
Brett Kuskie <fullaxx@gmail.com>
2022-12-27 06:39:58 -0700

    Update BareMetal64/001-core/03m-baremetal_modules_local.sh with EXCLUSIVE_PROCESS and nvidia-cuda-mps-control

--- BareMetal64/001-core/03m-baremetal_modules_local.sh
+++ BareMetal64/001-core/03m-baremetal_modules_local.sh
@@ -32,4 +32,18 @@ if [ -x /usr/bin/nvidia-persistenced ]; then
   echo "Starting nvidia-persistenced --user root ..."
   /usr/bin/nvidia-persistenced --user root
 fi
+
+# Set EXCLUSIVE_PROCESS with nvidia-smi
+if [ -x /usr/bin/nvidia-smi ]; then
+  echo "Setting GPU EXCLUSIVE_PROCESS with nvidia-smi ..."
+  /usr/bin/nvidia-smi -i 0 -c EXCLUSIVE_PROCESS
+  /usr/bin/nvidia-smi -i 1 -c EXCLUSIVE_PROCESS
+fi
+
+# Start nvidia-cuda-mps-control daemon
+if [ -x /usr/bin/nvidia-cuda-mps-control ]; then
+  echo "Starting nvidia-cuda-mps-control -d ..."
+  /usr/bin/nvidia-cuda-mps-control -d
+fi
+
 EOFF
