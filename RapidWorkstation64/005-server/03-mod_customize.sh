acdeadebb1dc7c7c40e5b1ad822f9e10457d6677
Brett Kuskie <fullaxx@gmail.com>
2023-03-17 04:06:04 -0600

    Update RapidWorkstation64/005-server/03-mod_customize.sh

--- RapidWorkstation64/005-server/03-mod_customize.sh
+++ RapidWorkstation64/005-server/03-mod_customize.sh
@@ -3,11 +3,10 @@
 source ../spec.sh
 source ./module_spec.sh
 
-### if [ -f ${DIR}/etc/rc.d/rc.samba ]; then
 if [ ${INCSAMBA} == "Y" ]; then
   sed -e 's@#!/bin/sh@#!/bin/bash@' -i ${DIR}/etc/rc.d/rc.samba
-  rm ${DIR}/usr/${LIBDIR}/krb5/plugins/kdb/samba.so
-  rm ${DIR}/usr/${LIBDIR}/service/kdc.so
+#  rm ${DIR}/usr/${LIBDIR}/krb5/plugins/kdb/samba.so
+#  rm ${DIR}/usr/${LIBDIR}/service/kdc.so
 fi
 
 if [ -f ${DIR}/etc/rc.d/rc.mysqld ]; then
@@ -17,13 +16,13 @@ fi
 PHPPLUG="${DIR}/usr/${LIBDIR}/php/extensions/snmp.so"
 if [ -f ${PHPPLUG} ]; then
   if [ "$INCNETSNMP" != "Y" ]; then
-    rm -f ${PHPPLUG}
+    rm ${PHPPLUG}
   fi
 fi
 
 TRAPPLUG="${DIR}/usr/${LIBDIR}/perl5/vendor_perl/auto/NetSNMP/TrapReceiver/TrapReceiver.so"
 if [ -f ${TRAPPLUG} ]; then
   if [ "$INCMARIADB" != "Y" ]; then
-    rm -f ${TRAPPLUG}
+    rm ${TRAPPLUG}
   fi
 fi
