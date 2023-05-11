96a4db937155eed5cc3f5c9e68ea19c622bf577e
Brett Kuskie <fullaxx@gmail.com>
2023-05-11 15:41:53 -0600

    Update RapidWorkstation64/010-xorg/03c-clean_harfbuzz.sh

--- RapidWorkstation64/010-xorg/03c-clean_harfbuzz.sh
+++ RapidWorkstation64/010-xorg/03c-clean_harfbuzz.sh
@@ -5,5 +5,6 @@ source ./module_spec.sh
 
 # Clean up harfbuzz, if we don't have cairo
 if [ "${INCCAIRO}" != "Y" ]; then
+  rm ${DIR}/usr/${LIBDIR}/libharfbuzz-cairo.so.*
   rm ${DIR}/usr/bin/hb-view
 fi
