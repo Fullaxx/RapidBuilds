c124baae820980bbc640792a0c2f50231eb60b35
Brett Kuskie <fullaxx@gmail.com>
2023-06-07 10:18:58 -0600

    Add rl_knet_max.sh

---
+++ RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rl_knet_max.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#    1MB =   1048576
+#    2MB =   2097152
+#    4MB =   4194304
+#    8MB =   8388608
+#   16MB =  16777216
+#   32MB =  33554432
+#   64MB =  67108864
+#  128MB = 134217728
+#  256MB = 268435456
+#  512MB = 536870912
+# 1024MB = 1073741824
+
+if [ `id -u` != "0" ]; then
+  echo "Got Root?"
+  exit 1
+fi
+
+increasekval()
+{
+	VALUE=`cat $2`
+	echo -n "$1: ${VALUE}"
+	if [ ${VALUE} -lt "$3" ]; then
+		echo "$3" >"$2"
+		SIZEMB=`calc "$3/1e6" | awk '{print $1}' | cut -d. -f1`
+		echo -n " (bumped to ${SIZEMB}MB)"
+	fi
+	echo
+}
+
+# cat /proc/sys/net/core/rmem_default
+# cat /proc/sys/net/core/wmem_default
+# cat /proc/sys/net/core/rmem_max
+# cat /proc/sys/net/core/wmem_max
+
+# cat /proc/sys/net/ipv4/tcp_mem
+# cat /proc/sys/net/ipv4/tcp_rmem
+# cat /proc/sys/net/ipv4/tcp_wmem
+# cat /proc/sys/net/ipv4/udp_mem
+# cat /proc/sys/net/ipv4/udp_rmem_min
+# cat /proc/sys/net/ipv4/udp_wmem_min
+
+COREDEF="1073741824"
+increasekval "rmem_default" "/proc/sys/net/core/rmem_default" "${COREDEF}"
+increasekval "wmem_default" "/proc/sys/net/core/wmem_default" "${COREDEF}"
+
+COREMAX="1073741824"
+increasekval "rmem_max" "/proc/sys/net/core/rmem_max" "${COREMAX}"
+increasekval "wmem_max" "/proc/sys/net/core/wmem_max" "${COREMAX}"
+
+UDPMIN="1073741824"
+increasekval "udp_rmem_min" "/proc/sys/net/ipv4/udp_rmem_min" "${UDPMIN}"
+increasekval "udp_wmem_min" "/proc/sys/net/ipv4/udp_wmem_min" "${UDPMIN}"
+
+echo "1073741824 1073741824 1073741824" >/proc/sys/net/ipv4/udp_mem
