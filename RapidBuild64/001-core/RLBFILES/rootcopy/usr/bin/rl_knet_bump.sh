2dc1bbc678bb9c58963bbc716b0a2ddd24c60284
Brett Kuskie <fullaxx@gmail.com>
2023-06-07 09:30:21 -0600

    Update rl_knet_bump.sh

--- RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rl_knet_bump.sh
+++ RapidBuild64/001-core/RLBFILES/rootcopy/usr/bin/rl_knet_bump.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-#   1MB =   1048576
-#   2MB =   2097152
-#   4MB =   4194304
-#   8MB =   8388608
-#  16MB =  16777216
-#  32MB =  33554432
-#  64MB =  67108864
-# 128MB = 134217728
-# 256MB = 268435456
+#    1MB =   1048576
+#    2MB =   2097152
+#    4MB =   4194304
+#    8MB =   8388608
+#   16MB =  16777216
+#   32MB =  33554432
+#   64MB =  67108864
+#  128MB = 134217728
+#  256MB = 268435456
+#  512MB = 536870912
+# 1024MB = 1073741824
 
 increasekval()
 {
 	VALUE=`cat $2`
 	echo -n "$1: ${VALUE}"
 	if [ ${VALUE} -lt "$3" ]; then
-		echo "$3" > "$2"
-		echo -n " (bumped to $3)"
+		echo "$3" >"$2"
+		SIZEMB=`calc "$3/1e6" | awk '{print $1}' | cut -d. -f1`
+		echo -n " (bumped to ${SIZEMB}MB)"
 	fi
 	echo
 }
@@ -33,19 +36,19 @@ increasekval()
 # cat /proc/sys/net/ipv4/udp_rmem_min
 # cat /proc/sys/net/ipv4/udp_wmem_min
 
-COREDEF="1048576"
+COREDEF="16777216"
 increasekval "rmem_default" "/proc/sys/net/core/rmem_default" "${COREDEF}"
 increasekval "wmem_default" "/proc/sys/net/core/wmem_default" "${COREDEF}"
 
-COREMAX="67108864"
+COREMAX="268435456"
 increasekval "rmem_max" "/proc/sys/net/core/rmem_max" "${COREMAX}"
 increasekval "wmem_max" "/proc/sys/net/core/wmem_max" "${COREMAX}"
 
-UDPMIN="16777216"
+UDPMIN="1073741824"
 increasekval "udp_rmem_min" "/proc/sys/net/ipv4/udp_rmem_min" "${UDPMIN}"
 increasekval "udp_wmem_min" "/proc/sys/net/ipv4/udp_wmem_min" "${UDPMIN}"
 
-echo "6187002 268435456 268435456" > /proc/sys/net/ipv4/udp_mem
+echo "134217728 1073741824 1073741824" >/proc/sys/net/ipv4/udp_mem
 
 # Just in case this happens to do anything at all
-echo 1 > /proc/sys/net/ipv4/route/flush
+echo 1 >/proc/sys/net/ipv4/route/flush
