1f2d78f90a10e4fb3f4855b09426aa20c601d422
Brett Kuskie <fullaxx@gmail.com>
2024-04-08 12:55:10 -0600

    Update to netdata-1.45.2

--- BareMetal64/baseoutput/06r-install_baremetal_mods.sh
+++ BareMetal64/baseoutput/06r-install_baremetal_mods.sh
@@ -23,6 +23,6 @@ verbosecopymodule ${PACKAGESDIR}/picdrv/000b-picdrv-3.9.8.56-${KERNVERS}-x86_64-
 # verbosecopymodule ${PACKAGESDIR}/dddvb/000d-dddvb-20230909-${KERNVERS}-x86_64-bksrc1.xzm ${BASEDIR}/
 
 # Docker Images
-verbosecopymodule ${PACKAGESDIR}/dimgs/netdata-1.44.3.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/dimgs/netdata-1.45.2.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/dimgs/portainer-2.19.4.xzm ${MODSDIR}/
 #verbosecopymodule ${PACKAGESDIR}/dimgs/dockly-230517.xzm ${MODSDIR}/
