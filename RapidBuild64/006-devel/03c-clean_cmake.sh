c0f5b6f58f8c0a512cbd80f79229f78fe065de2f
Brett Kuskie <fullaxx@gmail.com>
2024-04-24 10:10:33 +0000

    initial support for Qt6

--- RapidBuild64/006-devel/03c-clean_cmake.sh
+++ RapidBuild64/006-devel/03c-clean_cmake.sh
@@ -4,6 +4,6 @@ source ../spec.sh
 source ./module_spec.sh
 
 # Clean up cmake, if we dont have QT5
-if [ "${INCCMAKE}" == "Y" ] && [ "${INCQTFIVE}" != "Y" ]; then
+if [ "${INCCMAKE}" == "Y" ] && [ "${INCQTSIX}" != "Y" ]; then
   rm ${DIR}/usr/bin/cmake-gui
 fi
--- RapidWorkstation64/012-xapps/03c-clean_gstreamer.sh
+++ RapidWorkstation64/012-xapps/03c-clean_gstreamer.sh
@@ -8,22 +8,26 @@ if [ "${INCGSTREAMER}" == "Y" ]; then
 
 if [ "${INCCDPARANOIA}" != "Y" ]; then
 # gst-plugins-base-*.txz
-  rm ${DIR}/usr/lib64/gstreamer-1.0/libgstcdparanoia.so
+  rm ${DIR}/usr/${LIBDIR}/gstreamer-1.0/libgstcdparanoia.so
 fi
 
 if [ "${INCLIBVISUAL}" != "Y" ]; then
 # gst-plugins-base-*.txz
-  rm ${DIR}/usr/lib64/gstreamer-1.0/libgstlibvisual.so
+  rm ${DIR}/usr/${LIBDIR}/gstreamer-1.0/libgstlibvisual.so
 fi
 
 if [ "${INCLIBSOUP}" != "Y" ]; then
 # gst-plugins-good-*.txz
-  rm ${DIR}/usr/lib64/gstreamer-1.0/libgstsoup.so
+  rm ${DIR}/usr/${LIBDIR}/gstreamer-1.0/libgstsoup.so
 fi
 
 if [ "${INCTAGLIB}" != "Y" ]; then
 # gst-plugins-good-*.txz
-  rm ${DIR}/usr/lib64/gstreamer-1.0/libgsttaglib.so
+  rm ${DIR}/usr/${LIBDIR}/gstreamer-1.0/libgsttaglib.so
+fi
+
+if [ "${INCQTSIX}" != "Y" ]; then
+  rm ${DIR}/usr/${LIBDIR}/gstreamer-1.0/libgstqml6.so
 fi
 
 fi
