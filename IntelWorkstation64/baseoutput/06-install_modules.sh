2097fa6bd795ef979e170a6dcea6edebdb950941
Brett Kuskie <fullaxx@gmail.com>
2024-10-22 14:34:30 +0000

    Update to VirtualBox-7.0.22

--- IntelWorkstation64/baseoutput/06-install_modules.sh
+++ IntelWorkstation64/baseoutput/06-install_modules.sh
@@ -30,7 +30,7 @@ if [ "${INCVSCODE}" == "Y" ]; then
 fi
 
 # Copy any extra modules that don't need RDL
-verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-6.1.50-x86_64-1.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/VirtualBox/VirtualBox-7.0.22-x86_64-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/dbwalls/dbwalls-1920-221014.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/docker/docker-26.1.4-${ARCH}-1.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/mcast_xfer/mcastxfer-20230607-${ARCH}-bksrc1.xzm ${MODSDIR}/
