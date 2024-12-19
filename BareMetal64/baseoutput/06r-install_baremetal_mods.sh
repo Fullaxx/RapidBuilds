3352834b516e6c65bfa79c69ea89ca3ce56c7f14
Brett Kuskie <fullaxx@gmail.com>
2024-12-19 22:18:58 +0000

    Update to portainer-2.21.5

--- BareMetal64/baseoutput/06r-install_baremetal_mods.sh
+++ BareMetal64/baseoutput/06r-install_baremetal_mods.sh
@@ -24,6 +24,6 @@ verbosecopymodule ${PACKAGESDIR}/dddvb/000d-dddvb-20241027-${KERNVERS}-x86_64-bk
 
 # Docker Images
 verbosecopymodule ${PACKAGESDIR}/dimgs/netdata-1.47.5.xzm ${MODSDIR}/
-verbosecopymodule ${PACKAGESDIR}/dimgs/portainer-2.21.4.xzm ${MODSDIR}/
+verbosecopymodule ${PACKAGESDIR}/dimgs/portainer-2.21.5.xzm ${MODSDIR}/
 verbosecopymodule ${PACKAGESDIR}/dimgs/samba-241027.xzm ${MODSDIR}/
 #verbosecopymodule ${PACKAGESDIR}/dimgs/dockly-230517.xzm ${MODSDIR}/
