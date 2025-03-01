c9e6de761f5e75619420f8cb0b8b8dae2e6153e7
Brett Kuskie <fullaxx@gmail.com>
2025-03-01 11:18:26 +0000

    Update to chrome-133.0.6943.141

--- CudaWorkstation64/baseoutput/06-install_modules.sh
+++ CudaWorkstation64/baseoutput/06-install_modules.sh
@@ -17,7 +17,7 @@ verbosecopymodule ${PACKAGESDIR}/nvidia_driver/${KERNVERS}/011-nvidia-550.144.03
 
 # Chrome needs RDL
 if [ "${INCCHROME}" == "Y" ]; then
-  verbosecopymodule ${PACKAGESDIR}/chrome/chrome-132.0.6834.159-x86_64-1.xzm ${MODSDIR}/
+  verbosecopymodule ${PACKAGESDIR}/chrome/chrome-133.0.6943.141-x86_64-1.xzm ${MODSDIR}/
 fi
 
 # LibreOffice needs RDL
