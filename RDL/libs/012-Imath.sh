6468b5c1e2433616a6514a59104c24c79278e764
Brett Kuskie <fullaxx@gmail.com>
2025-04-16 22:33:54 +0000

    Add RDL/libs/012-Imath.sh

---
+++ RDL/libs/012-Imath.sh
@@ -0,0 +1,7 @@
+if [ "${INCIMATH}" == "Y" ]; then
+# Sat Apr 12 03:43:55 UTC 2025
+# Is this a hard req?
+# Is it just the python dynamic libraries?
+# I dont think will break anything if we dont include boost
+  INCBOOST="Y"
+fi
