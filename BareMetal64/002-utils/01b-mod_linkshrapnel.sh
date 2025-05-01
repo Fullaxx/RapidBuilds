f2c89c6b06f6ece000775f53adb660bb3dda4b7f
Brett Kuskie <fullaxx@gmail.com>
2025-05-01 09:48:37 +0000

    Remove bmon from BareMetal64/002-utils/01b-mod_linkshrapnel.sh

--- BareMetal64/002-utils/01b-mod_linkshrapnel.sh
+++ BareMetal64/002-utils/01b-mod_linkshrapnel.sh
@@ -9,10 +9,6 @@ linkpackage ${PACKAGESDIR}/lazy/lazygit-0.40.2-${ARCH}-1.xzm
 linkpackage ${PACKAGESDIR}/nvtop/nvtop-3.0.2-x86_64-1.xzm
 linkpackage ${PACKAGESDIR}/strace_standalone/strace-240325.xzm
 
-# bmon needs libconfuse
-linkpackage ${PACKAGESDIR}/libconfuse/libconfuse-3.2.2-${ARCH}-bksrc1.xzm
-linkpackage ${PACKAGESDIR}/bmon/bmon-4.0-${ARCH}-bksrc1.xzm
-
 # tshark needs libsmi
 linkpackage ${PACKAGESDIR}/libsmi/libsmi-0.5.0-${ARCH}-bksrc1.xzm
 linkpackage ${PACKAGESDIR}/wireshark/tshark-4.2.10-${ARCH}-bksrc1.xzm
